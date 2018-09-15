52067bf9f57cf44a3c603094aa749ed85c5a95da
wemlive <wangem@lesaas.cn>
2018-09-15 17:13:28 +0800

    增加了task定时任务线程功能

---
+++ src/main/java/com/answern/concurrency/concurrency/base/SchedulerConfig.java
@@ -0,0 +1,28 @@
+package com.answern.concurrency.concurrency.base;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+/**
+ * 需求名称: 将task线程设置为多线程服务<br/>
+ *
+ * @author [wem] <br/>
+ * 创建时间:[2018/9/15 17:10]  <br/>
+ * 版本:[v1.0]   <br/>
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        //线程池大小
+        scheduler.setPoolSize(10);
+        //线程名字前缀
+        scheduler.setThreadNamePrefix("spring-task-thread");
+        return scheduler;
+    }
+}
---
+++ src/main/java/com/answern/concurrency/concurrency/server/TastDemo.java
@@ -0,0 +1,41 @@
+package com.answern.concurrency.concurrency.server;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 需求名称:
+ * 类描述:[一句话描述该类的功能]<br/>
+ *
+ * @author [wem] <br/>
+ * 创建时间:[2018/9/15 17:03]  <br/>
+ * 版本:[v1.0]   <br/>
+ */
+@Component
+public class TastDemo {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    private int count=0;
+
+    /**
+     * demo 1
+     */
+    @Scheduled(cron="*/6 * * * * ?")
+    private void process(){
+        System.out.println("[" + Thread.currentThread().getName() + "]" + "this is scheduler task runing  "+(count++));
+    }
+
+
+
+    /**
+     * drmo 2
+     */
+    @Scheduled(fixedRate = 6000)
+    public void reportCurrentTime() {
+        System.out.println("[" + Thread.currentThread().getName() + "]" + "现在时间：" + dateFormat.format(new Date()));
+    }
+}
